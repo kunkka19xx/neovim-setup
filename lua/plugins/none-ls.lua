98dde969ae6a98924369163b1154d2d9b78280d5
kunkka19xx <nobita079x@gmail.com>
2024-09-15 18:37:53 +0900

    ruff does not work

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
         null_ls.builtins.formatting.black,
         -- null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.ruff,
+        -- null_ls.builtins.diagnostics.ruff,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.code_actions.impl,
       },
