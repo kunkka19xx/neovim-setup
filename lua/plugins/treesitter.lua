8099af24d986453c12b8b68c57ba9d7fa69be484
kunkka19xx <nobita079x@gmail.com>
2024-09-27 22:21:47 +0900

    add auto tag for html

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      -- auto install
-      auto_install = true,
-      -- add language you want to highlight in code
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "javascript",
-        "html",
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-        "java",
-        "json",
-        "zig",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			-- auto install
+			auto_install = true,
+			-- add language you want to highlight in code
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"javascript",
+				"html",
+				"go",
+				"gomod",
+				"gowork",
+				"gosum",
+				"java",
+				"json",
+				"zig",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
