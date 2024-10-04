f77699dbe14fe4945da7db8a75378664e643710b
kunkka19xx <nobita079x@gmail.com>
2024-10-04 18:00:29 +0900

    add view error diagnostic

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,3 +14,4 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- paste over highlight word
 vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
