a863f52bab4137f107cef2f1f46d0d752609e1dc
kunkka19xx <nobita079x@gmail.com>
2024-10-19 15:47:44 +0900

    fix tab for flutter

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -7,5 +7,14 @@ return {
   },
   config = function()
     require("flutter-tools").setup({}) -- use defaults
+    -- Set indentation to 2 spaces for Dart/Flutter files
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "dart",
+      callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.expandtab = true
+      end,
+    })
   end,
 }
