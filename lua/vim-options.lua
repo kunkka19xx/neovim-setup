e7a15ac23945a3306690c78125337e567eef93ee
kunkka19xx <nobita079x@gmail.com>
2024-11-13 22:17:16 +0900

    add copy file path

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = " "
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set cursorline")
-vim.api.nvim_set_option("clipboard","unnamed")
+vim.api.nvim_set_option("clipboard", "unnamed")
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 -- move selected lines
@@ -16,3 +16,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.opt.colorcolumn = "94"
+-- copy current file path (absolute) into clipboard
+vim.keymap.set("n", "<leader>cp", function()
+    local filepath = vim.fn.expand("%:p")
+    vim.fn.setreg("+", filepath)                        -- Copy to Neovim clipboard
+    vim.fn.system("echo '" .. filepath .. "' | pbcopy") -- Copy to macOS clipboard
+    print("Copied: " .. filepath)
+end, { desc = "Copy absolute path to clipboard" })
