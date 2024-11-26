88ee28c3e7dbae5ecd96626eb2a4e18b5e6637ac
kunkka19xx <nobita079x@gmail.com>
2024-11-26 12:07:38 +0900

    ln

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,8 @@ vim.g.mapleader = " "
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set cursorline")
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "white" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ead84e" })
 vim.api.nvim_set_option("clipboard", "unnamed")
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -13,13 +15,13 @@ vim.opt.incsearch = true
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- paste over highlight word
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", '"_dP')
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.opt.colorcolumn = "94"
 -- copy current file path (absolute) into clipboard
 vim.keymap.set("n", "<leader>cp", function()
-    local filepath = vim.fn.expand("%:p")
-    vim.fn.setreg("+", filepath)                        -- Copy to Neovim clipboard
-    vim.fn.system("echo '" .. filepath .. "' | pbcopy") -- Copy to macOS clipboard
-    print("Copied: " .. filepath)
+	local filepath = vim.fn.expand("%:p")
+	vim.fn.setreg("+", filepath) -- Copy to Neovim clipboard
+	vim.fn.system("echo '" .. filepath .. "' | pbcopy") -- Copy to macOS clipboard
+	print("Copied: " .. filepath)
 end, { desc = "Copy absolute path to clipboard" })
