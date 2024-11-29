d491c97bb0ad6054af12dd88518bea6db480ad67
kunkka19xx <nobita079x@gmail.com>
2024-11-29 14:46:11 +0900

    ud

--- lua/plugins/undo-tree.lua
+++ lua/plugins/undo-tree.lua
@@ -2,5 +2,6 @@ return {
   "mbbill/undotree",
   config = function ()
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.keymap.set('n', '<leader>uf', vim.cmd.UndotreeFocus)
   end
 }
